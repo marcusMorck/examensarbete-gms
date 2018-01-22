62bc95723894d0555eb296b51ea9699b40ca7402
Marcus Mörck <marcus.morck@gmail.com>
2018-01-22 18:34:25 +0100

    changes

---
+++ wp-content/themes/gms/assets/js/menu.js
@@ -0,0 +1,39 @@
+/**
+ * Created by marcus on 2018-01-22.
+ */
+(function($) {
+
+    /*$width = $( document ).width();
+
+
+    if ($( $width >= 900)){
+        $(".responsive-menu").hide();
+        $('.sidebar').show();
+        $(".menu").show();
+    }*/
+})( jQuery );
+
+
+jQuery( document ).ready(function( $ ) {
+
+    /*$width = $( document ).width();
+
+
+    if ($( $width >= 900)){
+        $(".responsive-menu").hide();
+        $('.sidebar').show();
+        $(".menu").show();
+    }
+    else{
+        $(".responsive-menu").show();
+        $('.sidebar').hide();
+        $(".menu").hide();
+    }
+
+    $('.fa-bars').on('click', function() {
+       $('.sidebar').toggle();
+       $('.menu').toggle();
+    })
+
+*/
+});
